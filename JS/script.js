38f1e432889fb7654ec461207cf56492db97b11d
AbdullRhman <abdullrhman27424@gmail.com>
2025-01-31 02:44:45 +0200

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,49 @@
+//Navigation menu ................
+
+
+let toggler = document.querySelector(".toggle");
+let nav = document.querySelector("nav");
+let close = document.querySelector(".close");
+
+toggler.onclick = function () {
+  nav.classList.add("open");
+};
+
+close.onclick = function () {
+  this.parentElement.classList.remove("open");
+};
+
+
+//Color the button on click........
+
+
+const navLinks = document.querySelectorAll('.nav1 a');
+const currentURL = window.location.href;
+
+navLinks.forEach(link => {
+  if (link.href === currentURL) {
+    link.classList.add('active'); 
+  }
+});
+
+
+//Scroll To Top button..............
+
+
+let span = document.querySelector(".up");
+
+window.onscroll = function () {
+  console.log(this.scrollY);
+  if (this.scrollY >= 1000) {
+    span.classList.add("show");
+  } else {
+    span.classList.remove("show");
+}
+};
+
+span.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
